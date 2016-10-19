9ba606c10be38a4c6a2d1d62e69dbdc6673d5a9d
Che B <Che@Che-no-MacBook-Air.local>
2016-10-19 17:03:55 -0400

    second commit

--- po17.py
+++ po17.py
@@ -1,8 +1,19 @@
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
 from flask import Flask, render_template, redirect, request, session
+import os
 import pg
 
-app = Flask('phonebook-16')
-db = pg.DB(dbname='student_db')
+db = pg.DB(
+    dbname=os.environ.get('student_db'),
+    host=os.environ.get('PG_HOST'),
+    user=os.environ.get('PG_USERNAME'),
+    passwd=os.environ.get('PG_PASSWORD')
+)
+
+tmp_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask('student', template_folder=tmp_dir)
 app.secret_key = "check"
 
 ###### HOME #####
